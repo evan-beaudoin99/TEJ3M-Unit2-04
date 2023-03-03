df3a1eba2c5befa47724fbac70a4fdf5e6dc7f6a
Evan Beaudoin <124371659+evan-beaudoin99@users.noreply.github.com>
2023-03-03 09:51:07 -0500

    Create RGB.ino

---
+++ RGB.ino
@@ -0,0 +1,33 @@
+/*
+
+  Created by: Evan Beaudoin
+  Created on: March 2023
+  Turns on the colors of an RGB light
+
+*/
+
+
+int redPin= 12;
+int greenPin = 11;
+int bluePin = 10;
+
+void setup() {
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+}
+
+void loop() {
+  setColor(255, 0, 0); // Red Color
+  delay(1000);
+  setColor(0, 255, 0); // Green Color
+  delay(1000);
+  setColor(0, 0, 255); // Blue Color
+  delay(1000);
+}
+
+void setColor(int redValue, int greenValue, int blueValue) {
+  analogWrite(redPin, redValue);
+  analogWrite(greenPin, greenValue);
+  analogWrite(bluePin, blueValue);
+}
